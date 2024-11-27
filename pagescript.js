5aaaf47efa55f527688d974abf1320884afa78ae
Johan Furuhjelm <31901041+johanfuruhjelm@users.noreply.github.com>
2024-11-28 00:11:42 +0100

    Manifest and first try at fixing copy button

--- pagescript.js
+++ pagescript.js
@@ -11,3 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
       buttons.forEach(button => button.classList.add("reading-mode")) // Add reading mode class to buttons
     }
   });
+
+function copyLink() {
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (result.state === "granted" || result.state === "prompt") {
+      let link = window.location.href
+      navigator.clipboard.writeText(link)
+    }
+    else{
+      alert("Clipboard permission denied")
+    }
+  });
+    
+}
